0a3b8c167676d9e5ce8b89b6af345817b4faf27d
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2023-07-23 04:20:27 +0100

    changed avatar

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230723.034320';
+const cacheName = 'chirpy-20230723.041716';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
